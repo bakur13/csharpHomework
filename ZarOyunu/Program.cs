a1437a520c6cc8060cb46f6681df77257c4d4e53
Hamza Uslu <108797599+bakur13@users.noreply.github.com>
2023-02-22 08:33:39 +0300

    Update Program.cs

--- ZarOyunu/Program.cs
+++ ZarOyunu/Program.cs
@@ -1,32 +1,34 @@
-﻿using System;
+using System;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        Random random = new Random();
+        Random rnd = new Random();
+
         int userNumber;
 
         Console.Write("1-6 arası bir sayı girin: ");
+
         while (!int.TryParse(Console.ReadLine(), out userNumber) || userNumber < 1 || userNumber > 6)
         {
             Console.Write("Geçersiz giriş. Lütfen 1-6 arası bir sayı girin: ");
         }
 
-        int dice1, dice2;
+        int zar1, zar2;
         int tries = 0;
 
         while (true)
         {
-            dice1 = random.Next(1, 7);
-            dice2 = random.Next(1, 7);
+            zar1 = rnd.Next(1, 7);
+            zar2 = rnd.Next(1, 7);
             tries++;
 
-            Console.WriteLine("{0}. deneme: {1} ve {2}", tries, dice1, dice2);
+            Console.WriteLine("{0}. deneme: {1} ve {2}", tries, zar1, zar2);
 
-            if (dice1 + dice2 == 2 * userNumber)
+            if ( zar2 ==  userNumber&&zar1==userNumber)
             {
-                Console.WriteLine("{0} ve {1} atıldı, tebrikler! {2} denemede bildiniz.", dice1, dice2, tries);
+                Console.WriteLine("{0} ve {1} atıldı, tebrikler! {2} denemede bildiniz.", zar1, zar2, tries);
                 break;
             }
         }
